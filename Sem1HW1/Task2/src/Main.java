a947b7bbfc9acbceb89c98f88ce069ee6473f70d
AntonSominskii <101739742+AntonSominskii@users.noreply.github.com>
2023-06-21 15:35:37 +0300

    Empty projects added
    Empty projects added

---
+++ Sem1HW1/Task2/src/Main.java
@@ -0,0 +1,6 @@
+// 2) Вывести все простые числа от 1 до 1000
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ Sem1HW1/Task3/src/Main.java
@@ -0,0 +1,6 @@
+// 3) Реализовать простой калькулятор
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
